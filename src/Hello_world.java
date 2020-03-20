be9a39eda010d9db0e43396e7d00b56be77b38fe
eric smith <eric.smith@n.com>
2020-03-20 04:32:56 -0400

    First

---
+++ src/Hello_world.java
@@ -0,0 +1,15 @@
+
+
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for (i=0; i<10;i++)
+		{
+            System.out.println("Hello World!");
+		}
+	}
+
+}
